de330319ebbe89ef48e33f8fb9a5d6d50b06e70d
Hammurabi <koberstine@hotmail.com>
2021-07-09 13:32:23 -0400

    initial routes

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
 
 app.get('/notes', (req, res) => {
