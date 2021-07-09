75964157d23b97058844400d4106f6c15fa8d3a4
Hammurabi <koberstine@hotmail.com>
2021-07-09 13:10:09 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
