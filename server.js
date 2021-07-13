6d703ec45257ccdd4f4fa0fc6825eb290874b7ad
Hammurabi <koberstine@hotmail.com>
2021-07-13 13:55:13 -0400

    restored paths without static folder

--- server.js
+++ server.js
@@ -7,10 +7,9 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
 });
 
 app.get('/api/notes', (req, res) => {
@@ -20,7 +19,7 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
