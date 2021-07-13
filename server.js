c3b8fdec712e25c4e9dee7235925fd498136ecce
Hammurabi <koberstine@hotmail.com>
2021-07-13 15:37:56 -0400

    added static public folder

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
