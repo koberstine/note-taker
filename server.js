b7b57027d2d9f1dca9900feeba9be59672ddfee6
Hammurabi <koberstine@hotmail.com>
2021-07-13 15:42:44 -0400

    added static public folder

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.get('/api/notes', (req, res) => {
       return parsedData;
     });
 });
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
-});
-
+ 
 app.post('/api/notes', (req, res) => {
     // placeholder for POST route
     res.send('POST request to page');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(PORT, () => {
